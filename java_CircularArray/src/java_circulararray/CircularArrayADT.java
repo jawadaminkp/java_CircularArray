e82e1cd5484aac576cabdd8bab577115d165f3c2
jawad@jawad.com <59015763+jawadaminkp@users.noreply.github.com>
2021-01-02 17:14:54 +0500

    Circular Array ADT code is completed

--- java_CircularArray/src/java_circulararray/CircularArrayADT.java
+++ java_CircularArray/src/java_circulararray/CircularArrayADT.java
@@ -3,6 +3,23 @@ package java_circulararray;
 
 public class CircularArrayADT {
     
+    public void print(char[] a, int ind, int n){
+        
+        
+        char[] b = new char[(n*2)];
+        
+        for(int i=0; i < n; i++ ){
+            b[i]=b[n+i]=a[i];
+        }
+        
+        
+        for(int i=ind; i < n+ind;i++){
+            System.out.println(b[i]+" ");
+        }
+        
+        
+        
+    }
     
     
 }
