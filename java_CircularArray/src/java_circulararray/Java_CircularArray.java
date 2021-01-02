1294e1ef3d4f437618b9052e1fea7a49971cf950
jawad@jawad.com <59015763+jawadaminkp@users.noreply.github.com>
2021-01-02 17:21:28 +0500

    driver class code completed successfully

--- java_CircularArray/src/java_circulararray/Java_CircularArray.java
+++ java_CircularArray/src/java_circulararray/Java_CircularArray.java
@@ -1,10 +1,24 @@
 
 package java_circulararray;
+import java.util.*;
+
 
 public class Java_CircularArray {
 
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        CircularArrayADT arrayprint= new CircularArrayADT();
+       
+        char[] circularArray = {'A','B','C','D','E','F'};
+        
+     
+        
+        int size= 6;
+        
+        
+        
+        arrayprint.print(circularArray, 4, size);
+        
     }
     
 }
