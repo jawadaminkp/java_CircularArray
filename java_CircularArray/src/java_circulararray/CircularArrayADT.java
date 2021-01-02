a868c72947c64472acbbd428826fe767b232e9b8
jawad@jawad.com <59015763+jawadaminkp@users.noreply.github.com>
2021-01-02 17:21:05 +0500

    ADT coded completed successfully

--- java_CircularArray/src/java_circulararray/CircularArrayADT.java
+++ java_CircularArray/src/java_circulararray/CircularArrayADT.java
@@ -8,13 +8,16 @@ public class CircularArrayADT {
         
         char[] b = new char[(n*2)];
         
+        //populating auxillary array with data of original array data for twice
         for(int i=0; i < n; i++ ){
             b[i]=b[n+i]=a[i];
         }
         
         
+        
+        //printing data from the given index to the index-1 postion.
         for(int i=ind; i < n+ind;i++){
-            System.out.println(b[i]+" ");
+            System.out.print(b[i]+" ");
         }
         
         
