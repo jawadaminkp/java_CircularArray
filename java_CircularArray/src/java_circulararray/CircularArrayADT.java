7612e5a1121613d0a683a168295f88a2ff56573a
Jawad Amin <59015763+jawadaminkp@users.noreply.github.com>
2021-01-04 14:15:01 +0500

    Format set

--- java_CircularArray/src/java_circulararray/CircularArrayADT.java
+++ java_CircularArray/src/java_circulararray/CircularArrayADT.java
@@ -1,28 +1,20 @@
 package java_circulararray;
 
-
 public class CircularArrayADT {
     
     public void print(char[] a, int ind, int n){
         
-        
         char[] b = new char[(n*2)];
         
-        //populating auxillary array with data of original array data for twice
+        //populating auxillary array with data
+        //of original array data for twice
         for(int i=0; i < n; i++ ){
             b[i]=b[n+i]=a[i];
         }
         
-        
-        
         //printing data from the given index to the index-1 postion.
         for(int i=ind; i < n+ind;i++){
             System.out.print(b[i]+" ");
-        }
-        
-        
-        
-    }
-    
-    
+        } 
+    }  
 }
